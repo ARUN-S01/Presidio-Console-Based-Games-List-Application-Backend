cda459732703892887f8ff4cf16b8550f72d4ac0
ARUN-S01 <a2r0u0n2@gmail.com>
2023-11-19 13:55:40 +0530

    Code Updates

--- server.js
+++ server.js
@@ -1,48 +1,56 @@
+// Importing Libraries
 const express = require('express')
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
-const cors = require('cors');
-const uri = "mongodb+srv://a2r0u0n2:asdf0110@game.z5dyslm.mongodb.net/?retryWrites=true&w=majority";
+const cors = require('cors'); // Cross-Origin Resource Sharing (Middleware)
+
+const uri = "mongodb+srv://a2r0u0n2:asdf0110@game.z5dyslm.mongodb.net/?retryWrites=true&w=majority"; // Connection String
 const client = new MongoClient(uri);
-client.connect(err =>{
+
+client.connect(err => { // MongoDB Connection
     console.log("Connected to DB");
 });
 
+// Configuring API
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
 
-
-app.post('/insert',async (request,response) => {
+// CRUD Methods
+// Insert
+app.post('/insert', async (request, response) => {
     console.log(request.body);
     const collection_ = client.db("gameList").collection("games");
     const result = await collection_.insertOne(request.body);
-    response.status(200).send("Success"); 
+    response.status(200).send("Success");
 });
 
-app.put('/update',async (req,res)=>{
+// Update
+app.put('/update', async (req, res) => {
     const col = client.db("gameList").collection("games");
-    const filter = {name:req.body.name, year:req.body.year};
+    const filter = { name: req.body.name, year: req.body.year };
     const up_doc = {
-        $set:{
-            name:req.body.name,
-            isFav:req.body.isFav
+        $set: {
+            name: req.body.name,
+            isFav: req.body.isFav
         }
     }
-    const result = await col.updateOne(filter,up_doc);
-    res.status(200).send(result); 
+    const result = await col.updateOne(filter, up_doc);
+    res.status(200).send(result);
 })
 
-app.get('/get',async (request,response) => {
+// Get
+app.get('/get', async (request, response) => {
     const col = client.db("gameList").collection("games");
     const result = await col.find({}).toArray();
     response.status(200).send(result);
 })
 
-app.delete('/del',async (req,res) =>{
+// Delete
+app.delete('/del', async (req, res) => {
     const col = client.db("gameList").collection("games");
-    var myquery = { name: req.body.name , year: req.body.year};
+    var myquery = { name: req.body.name, year: req.body.year };
     const rsult = await col.deleteOne(myquery);
     res.status(200).send(rsult);
 
